75bc4db3315aa09cfb7351aae5ab4954cdb80c2e
MurakamiShinyu <murakami@vivliostyle.org>
2023-05-23 14:59:20 +0900

    add files

---
+++ text/backmatter.md
@@ -0,0 +1,41 @@
+## 著者略歴 {.break-recto}
+
+<div class="biography">
+
+**村上真完**<br>
+昭和7（1932）年青森県黒石市生まれ<br>
+東北大学名誉教授<br>
+文学博士（東北大学）<br>
+**村上ヤエ**<br>
+昭和7（1932）年岩手県盛岡市生まれ<br>
+元高校、専門学校教師<br>
+文学修士（東北大学）<br>
+
+</div>
+
+## 海外の旅〔上〕{#colophon}
+
+|||
+| ----- | ------------------- |
+|発　行|2023年3月3日　第1刷|
+|著　者|村上真完・ヤエ|
+|制　作|一般社団法人ビブリオスタイル|
+|発行人|村上真完・ヤエ
+|||
+|印刷所|欧文印刷株式会社|
+
+<div class="map-source">
+
+本書収録の地図は下記のデータを使用しました。記して感謝いたします。
+- 一／二：白地図専門店
+    - https\://www\.freemap.jp/item/europe/europe.html
+- 三／四／五：d-maps.com 
+    - https\://d-maps\.com/m/america/amnord/amnord10.pdf
+    - https\://d-maps\.com/m/asia/asie_est_sud/asie_est_sud08.pdf
+    - https\://d-maps\.com/m/asia/thailande/thailande08.pdf
+    - https\://d-maps\.com/m/asia/birmanie/birmanie14.pdf
+    - https\://d-maps\.com/m/asia/india/inde/inde24.pdf
+    - https\://d-maps\.com/m/asia/india/tamil/tamil14.pdf
+    - https\://d-maps\.com/m/asia/srilanka/srilanka24.pdf
+
+</div>
---
+++ vivliostyle.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: '海外の旅〔上〕', // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
+  author: '村上真完・ヤエ', // default to `author` in `package.json` or undefined.
+  language: 'ja',
+  // size: 'A5',
+  theme: 'theme/theme.css', // .css or local dir or npm package. default to undefined.
+  // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
+  entry: [
+    "text/index.md",
+    "text/chap01.md",
+    "text/chap02.md",
+    "text/chap03.md",
+    "text/chap04.md",
+    "text/chap05.md",
+    "text/backmatter.md",
+    // 'manuscript.md', // `title` is automatically guessed from the file (frontmatter > first heading).
+    // {
+    //   path: 'epigraph.md',
+    //   title: 'Epigraph', // title can be overwritten (entry > file),
+    //   theme: '@vivliostyle/theme-whatever', // theme can be set indivisually. default to the root `theme`.
+    // },
+    // 'glossary.html', // html can be passed.
+  ], // `entry` can be `string` or `object` if there's only single markdown file.
+  // toc: true, // whether generate and include toc.html or not (does not affect manifest.json), default to `true`. if `string` given, use it as a custom toc.html.
+  // cover: './cover.png', // cover image. default to undefined.
+  // workspaceDir: '.vivliostyle',
+  http: true,
+  timeout: 1000000,
+}
